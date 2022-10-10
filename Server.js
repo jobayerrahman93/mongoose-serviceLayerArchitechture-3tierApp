3e9da781ab1b399171ff169752037f6217e82231
jobayercse71 <jobayercse71.com>
2022-10-11 00:58:13 +0600

    mongoose connection added

--- Server.js
+++ Server.js
@@ -1,8 +1,15 @@
-const dotenv = require('dotenv');
-const app = require('./index')
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = require('./index');
 const port = process.env.PORT || 4000;
 
 
+
+
+mongoose.connect(process.env.LOCAL_DB).then(()=>{
+  console.log('Mongoose is connected');
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
-})
+});
